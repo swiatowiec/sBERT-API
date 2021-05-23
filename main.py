00ffc95efeabc80760d1c8226809fbecd6cafa53
swiatowiec <katarzynaswiatowiec@gmail.com>
2021-05-23 17:39:32 +0200

    Create sentence embedding endpoint

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from sBERT import get_embedding
+from models import SentenceEmbedding
+from typing import List
+
+
+app = FastAPI()
+
+@app.post("/sentences_transformator", response_model=List[SentenceEmbedding])
+async def create_item(sentences: List[str]):
+    embeddings = get_embedding(sentences)
+    return embeddings
+
+
---
+++ sBERT.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+from models import SentenceEmbedding
+
+def get_embedding(sentences):
+    model = SentenceTransformer('paraphrase-distilroberta-base-v1')
+    embeddings = model.encode(sentences)
+    result = []
+    for sentence, embedding in zip(sentences, embeddings):
+        result.append(SentenceEmbedding( sentence=sentence, embedding=embedding.tolist()))
+    return result
