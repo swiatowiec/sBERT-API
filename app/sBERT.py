ccf11e2b55e24c4072c1adcadd0441ed6615d5f7
swiatowiec <katarzynaswiatowiec@gmail.com>
2021-05-24 21:38:00 +0200

    Add docstring to get_embedding

--- app/sBERT.py
+++ app/sBERT.py
@@ -1,7 +1,11 @@
 from sentence_transformers import SentenceTransformer
 from .models import SentenceEmbeddingOutput
+from typing import List
 
-def get_embedding(sentences):
+def get_embedding(sentences: List[str]):
+    """
+    Get sentences embeddings using sBERT.
+    """
     model = SentenceTransformer('paraphrase-distilroberta-base-v1')
     embeddings = model.encode(sentences)
     result = []
