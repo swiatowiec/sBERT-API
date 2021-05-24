35fc70ac26d57803057a6d80c96699dce2115c06
swiatowiec <katarzynaswiatowiec@gmail.com>
2021-05-24 21:55:34 +0200

    Docstring update

--- app/main.py
+++ app/main.py
@@ -7,10 +7,10 @@ from typing import List
 app = FastAPI()
 
 @app.post("/sentences_transformator", response_model=List[SentenceEmbeddingOutput])
-def create_item(request: List[str]):
+def sentences_transformator(request: List[str]):
     """
-    Create an item with the information:
-    - **sentence**: a sentence to transform as string
+    Transform list of sentences to sBERT embedding and for each sentence return:
+    - **sentence**: a transformed sentence as string
     - **embedding**: a sentence embedding as list
     """
     embeddings = get_embedding(request)
